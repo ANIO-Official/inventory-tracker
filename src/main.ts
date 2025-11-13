0bb9272bc35b938520c466cc4ec6250869112d2f
Amanda Ogletree <amandaogletree19@gmail.com>
2025-11-13 17:26:28 -0500

    Imported PhysicalProduct, Digital Product, and calculateTax. Instanced 1 new product for each class. Created a for of loop to list all product details and final price.

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import PhysicalProduct from "./models/PhysicalProduct";
+import DigitalProduct from "./models/DigitalProduct";
+import calculateTax from "./utils/taxCalculator";
+
+const clearWaterBottle = new PhysicalProduct ("SK257SR", "Clear Water Bottle", 1.99,0.5)
+const how2MakeMuffins = new DigitalProduct ("SK2567SG", "How 2 Make Muffins: The Sequel", 11.49, 10.53)
+
+const products = [clearWaterBottle, how2MakeMuffins]
+
+function listProducts(){
+    for (let i of products){
+       console.log(`About this product ${i.displayDetails()} It's price plus tax is ${calculateTax(i)}.`) 
+    }
+}
+
+listProducts()
